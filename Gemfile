8783df16f64a3e0d911d6c0f667b4ee1338e4a1e
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-06 16:01:11 +0900

    Devise-install/view-file

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "haml"
 gem 'erb2haml'
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-
   root 'messages#index'
+  devise_for :users
+  get 'user/new'
+
+  get 'group/new'
+
+
 
 end
