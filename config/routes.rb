354ebcaa84cbae9796deb1ff9829ed0119a80d39
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-07 20:35:58 +0900

    group_index 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only:[:new, :edit, :create,:update]  do
     resources :messages, only: [:index, :create]
 
   end
-
 end
