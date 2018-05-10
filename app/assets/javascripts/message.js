9a5acd6ea3b41725cc82ba6ef066cc30c1a601c1
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-10 17:51:54 +0900

    コード微修正
    読みやすさを改善

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,38 +1,37 @@
 $(function(){
   function buildHTML(message){
-     var img =" ";
-    if (message.image){
-      var img = `<img src = ${message.image} class: "lower-message_image">`
+  var img =" ";
+  if (message.image){
+    var img = `<img src = ${message.image} class: "lower-message_image">`
     }
-    var html = `<div class="right-content__messages__box">
-                  <div class="right-content__messages__box__username">
+    var html = `<div class=" right-content__messages__box">
+                  <div class= "right-content__messages__box__username">
                     <h4>${message.user_name}</h4>
                    </div>
-                  <div class="right-content__messages__box__time">
+                  <div class= "right-content__messages__box__time">
                     <p>${message.created_at}</p>
                   </div>
-                  <div class="right-content__messages__box__content">
+                  <div class= "right-content__messages__box__content">
                     <p>${message.content}</p>
                        ${img}
                    </div>
-                </div>
-`
+                </div>`
     return html;
   }
+
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
-      url:url,
-      type:"POST",
-      data:formData,
-      dataType:"json",
-      processData:false,
-      contentType:false
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
     })
-
- .done(function(data){
+  .done(function(data){
       var html = buildHTML(data);
       $('.right-content__messages__container').append(html)
       $('.right-content__messages__container').animate({scrollTop: $('.right-content__messages__container')[0].scrollHeight},'fasts')
@@ -42,7 +41,6 @@ $(function(){
     })
   .fail(function(){
     alert('error');
-
     })
   })
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-before_action :set_group, only: [:edit, :update]
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -30,11 +31,12 @@ before_action :set_group, only: [:edit, :update]
 
 private
  def group_params
-  params.require(:group).permit(:name, {:user_ids => []})
+  params.require(:group).permit(:name, {:user_ids []})
  end
- def set_group
+
+  def set_group
     @group = Group.find(params[:id])
   end
 end
-
 end
+
