c4b40b40850aa4369849c1fe84875c1194474f96
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-11 18:53:56 +0900

    コード微修正
    ・groups_controller.rbのbefore_action :set_groupから:indexを削除
    ・検索結果からログインユーザーを排除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    @users.delete(current_user.id)
     respond_to do |format|
       format.html
       format.json
