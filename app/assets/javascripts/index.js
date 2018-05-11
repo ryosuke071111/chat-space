45c608512a95abe3b4ef92f96db0969355a356f9
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-11 17:35:41 +0900

    add/remove member given by search result and current members

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
 $(function() {
 
-var search_list= $("#chat-group-users");
+var search_list = $("#user-search-result");
 
 function appendUser(user){
   var html = `<div class="chat-group-user clearfix">
@@ -10,9 +10,18 @@ function appendUser(user){
   search_list.append(html);
 }
 
+function appendNewUser(id, name){
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id} data-id=${id} '>
+                <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'>${name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+ return html;
+}
 
 
   $("#user-search-field").on("keyup", function(){
+    $("#user-search-result").empty();
     var input = $("#user-search-field").val();
     $.ajax({
       type: "GET",
@@ -20,17 +29,34 @@ function appendUser(user){
       data: { keyword: input },
       dataType: "json"
     })
-
     .done(function(users){
-      $(".chat-group-user").remove();
       if(users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
         });
       }
      })
-   .fail(function(){
-    alert("ユーザー検索に失敗しました");
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
     })
   })
+
+  $("#user-search-result").on("click",".chat-group-user__btn--add", function(){
+    var id = $(this).attr("data-user-id");
+    var name = $(this).attr("data-user-name");
+    var addNewUser = appendNewUser(id, name);
+    $('#chat-group-users').append(addNewUser);
+    $(this).parent('.chat-group-user').remove();
+  });
+
+  $("#chat-group-users").on("click",".user-search-remove", function(){
+  $(this).parent().remove();
+
+  });
+
+
+
+
 });
+
+
