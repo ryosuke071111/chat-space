032481ca90f985a6add32929958e6d3dfc7d57a2
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-13 21:26:46 +0900

    devise-revise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
+  post "/" => "groups#index"
   resources :users, only: [:edit, :update, :index]
-  resources :groups, only:[:new, :edit, :create,:update]  do
+  resources :groups, only:[:new, :edit, :create,:update, :index]  do
     resources :messages, only: [:index, :create]
   end
 end
