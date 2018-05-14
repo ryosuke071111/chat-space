aa8f0bcaf5efa0c8fdc8a061a587f591a6b02a26
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-14 19:46:45 +0900

    aa

--- spec/factories/messages.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :message do
-    content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/2018022700004_1.jpg")
-    user
-    group
-  end
-end
